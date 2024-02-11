0f76e377729448d1161bc6ed6eaa59eadc31c88c
Kozhan Nurassyl <nurasil21.07@mail.ru>
2024-02-11 15:32:10 +0600

    init commit

---
+++ src/main/java/com/example/stellarinvestment/StellarInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.stellarinvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarInvestmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StellarInvestmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stellarinvestment/StellarInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stellarinvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellarInvestmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
